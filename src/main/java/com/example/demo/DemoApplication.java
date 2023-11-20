441fb0eefa0117738358cddb7fcad0d1d83eebaa
Vinod Kumar <vinod.kumar@360factors.com>
2023-11-20 15:23:43 +0500

    file updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import java.util.*;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		//String name = "Vinod";
 		System.out.println("DemoApplication");
 		System.out.println("Vinod Kumar");
 	}
